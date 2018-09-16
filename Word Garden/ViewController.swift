f5b8ae21634affbc86a3b11531190f6e66812fa9
Mary Nugent <nugentb@bc.edu>
2018-09-16 12:16:12 -0400

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Brennan Nugent on 9/16/18.
+//  Copyright © 2018 Brennan Nugent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
